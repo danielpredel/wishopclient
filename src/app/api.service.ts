2634b9a99b21f18be04c4937c23a7edb1b675dc5
danielpredel <danielpreciadodelgadillo@gmail.com>
2023-12-03 20:25:08 -0600

    Initial commit

---
+++ src/app/api.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+  private apiUrl = 'http://localhost:3000/product/search/';
+
+  constructor(private http: HttpClient) { }
+  buscar(keyword: string) {
+    return this.http.get(this.apiUrl + keyword);
+  }
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ApiService } from './api.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'clientewishop';
+
+  keyword: string = '';
+  products: any = null;
+  datos: any = null;
+  success: boolean = false;
+
+  constructor(private apiService: ApiService){}
+
+  buscar(){
+    this.apiService.buscar(this.keyword).subscribe(
+      (response) => {
+        this.datos = response;
+        this.success = this.datos?.success;
+        if(this.success === true){
+          this.products = this.datos?.products;
+        }
+        console.log(this.success);
+        console.log(this.products);
+      },
+      (error) => {
+        console.error('Error en la petición GET:', error);
+        // alert('error' + error);
+      }
+    );
+  }
 }
